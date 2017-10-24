374a84b6a03ed5ed0004cdbb62b6ffe4168c0f01
lilschmitz <KathrinLSchmitz@gmail.com>
2017-10-24 13:23:22 +0100

    Changes to the worker.py

--- worker.py
+++ worker.py
@@ -1,15 +1,19 @@
 import os
-
+import urlparse
 import redis
 from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDISTOGO_URL', 'redis://lifestylefitnesscoaching.heroku.com')
+redis_url = os.getenv('REDISTOGO_URL')
+if not redis_url:
+    raise RuntimeError('Set up Redis To Go first.')
 
-conn = redis.from_url(redis_url)
+urlparse.uses_netloc.append('redis')
+url = urlparse.urlparse(redis_url)
+conn = redis(host=url.hostname, port=url.port, db=0, password=url.password)
 
 if __name__ == '__main__':
-    with Connection(conn):
-        worker = Worker(map(Queue, listen))
-        worker.work()
+with Connection(conn):
+    worker = Worker(map(Queue, listen))
+    worker.work()
