bc02f27eaed274d6b241a415187ee40a28855f0f
lilschmitz <KathrinLSchmitz@gmail.com>
2017-10-24 13:25:32 +0100

    Indentation fix in worker.py

--- worker.py
+++ worker.py
@@ -14,6 +14,6 @@ url = urlparse.urlparse(redis_url)
 conn = redis(host=url.hostname, port=url.port, db=0, password=url.password)
 
 if __name__ == '__main__':
-with Connection(conn):
-    worker = Worker(map(Queue, listen))
-    worker.work()
+    with Connection(conn):
+        worker = Worker(map(Queue, listen))
+        worker.work()
