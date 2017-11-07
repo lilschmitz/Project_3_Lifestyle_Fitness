6842189cc4243c647443ad21e0bfab1b0117f7cf
lilschmitz <KathrinLSchmitz@gmail.com>
2017-11-07 14:24:37 +0000

    Another try in urls.py syntax

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-import .views
+from .views import post_list, post_detail, new_post, edit_post
 
 urlpatterns = [
-    url(r'^posts/$', views.post_list, name='post_list'),
-    url(r'^posts/(?P<id>\d+)/$', views.post_detail, name='post_detail'),
-    url(r'^posts/new/$', views.new_post, name='new_post'),
-    url(r'^posts/(?P<id>\d+)/edit$', views.edit_post, name='edit_post'),
+    url(r'^posts/$', post_list, name='post_list'),
+    url(r'^posts/(?P<id>\d+)/$', post_detail, name='post_detail'),
+    url(r'^posts/new/$', new_post, name='new_post'),
+    url(r'^posts/(?P<id>\d+)/edit$', edit_post, name='edit_post'),
 ]
