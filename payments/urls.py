b40cd5bbbcda7aa1cab6bff42910bbcd27913263
lilschmitz <KathrinLSchmitz@gmail.com>
2017-11-08 12:41:33 +0000

    Changes to urls.py in payments app

--- payments/urls.py
+++ payments/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import buy_now
+from payments.views import buy_now
 
 urlpatterns = [
     url(r'^buy_now/(?P<id>\d+)', buy_now, name='buy_now_stripe'),
